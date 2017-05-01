67a5a03d4a1126d1c140b519b6f2b4294f67ab93
Orlando <orlandojsjr@gmail.com>
2017-05-01 15:02:47 -0300

    migrate on

--- src/main/scala/com/movieticket/restapi/utils/Migration.scala
+++ src/main/scala/com/movieticket/restapi/utils/Migration.scala
@@ -7,15 +7,15 @@ import org.flywaydb.core.Flyway
 trait Migration extends Config {
 
   private val flyway = new Flyway()
-  //flyway.setDataSource(databaseUrl, databaseUser, databasePassword)
+  flyway.setDataSource(databaseUrl, databaseUser, databasePassword)
 
   def migrate() = {
-    //flyway.migrate()
+    flyway.migrate()
   }
 
   def reloadSchema() = {
-    //flyway.clean()
-    //flyway.migrate()
+    flyway.clean()
+    flyway.migrate()
   }
 
 }
