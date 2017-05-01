a92121c3ef3ea85229c843f32123a7d55a067677
Orlando <orlandojsjr@gmail.com>
2017-05-01 13:15:41 -0300

    conf

--- src/main/scala/com/movieticket/restapi/http/Resource.scala
+++ src/main/scala/com/movieticket/restapi/http/Resource.scala
@@ -7,23 +7,7 @@ import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.server.{Directives, Route}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
-
 trait Resource extends Directives with SprayJsonSupport {
-
-  def completeWithLocationHeader[T](resourceId: Future[Option[T]], ifDefinedStatus: Int, ifEmptyStatus: Int): Route =
-    onSuccess(resourceId) {
-      case Some(t) => completeWithLocationHeader(ifDefinedStatus, t)
-      case None => complete(ifEmptyStatus, None)
-    }
-
-  def completeWithLocationHeader[T](status: Int, resourceId: T): Route =
-    extractRequestContext { requestContext =>
-      val request = requestContext.request
-      val location = request.uri.copy(path = request.uri.path / resourceId.toString)
-      respondWithHeader(Location(location)) {
-        complete(status, None)
-      }
-    }
 
   def complete[T: ToResponseMarshaller](resource: Future[Option[T]]): Route =
     onSuccess(resource) {
